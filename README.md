2f39c27f6d1d10505aceb71a41bb8926f7c8beff
Tai NA <taina0407@gmail.com>
2017-02-03 01:01:30 +0700

    Bumb version

--- README.md
+++ README.md
@@ -1,53 +1,36 @@
-# node-account-kit
+# node-accountkit
 Account Kit helps you quickly register for apps using just your phone number or email address — no password needed. It's reliable, easy to use and gives you a choice about how you sign up for apps.
 
-node-account-kit is a nodeJS module to handle account-kit server side implementation.
+node-accountkit is a nodeJS module to handle accountkit server side implementation.
 
-Full documentation for account kit can be found here [http://l.sgoyal.net/2ct7zQh](https://developers.facebook.com/docs/accountkit/web)
+Full documentation for account kit can be found here [https://developers.facebook.com/docs/accountkit/web](https://developers.facebook.com/docs/accountkit/web)
 
 ### Version
-1.0.0
+1.1.0
 
-### DEMO
-
-Click [http://l.sgoyal.net/accountkit](http://l.sgoyal.net/accountkit "account-kit demo implementation") to see running demo.
-
-Demo Code available here [ https://github.com/sgoyalnet/account-kit-app ](https://github.com/sgoyalnet/account-kit-app)
-
-### Features
-
-  - Generate randon non gussable string which can be passed to front end javascript file to be used for csrf.
-  - One shot function to authenticate and generate access token and return user information.
-  - support require app secret option.
-  - User data removal support.
- 
 ### Installation
 
 ```sh
-npm install node-account-kit
+npm install node-accountkit
 ```
 ### Usages
 
 **Step 1** Include module to your server.js
 ```javascript
-var accountkit = require ('node-account-kit');
+var accountkit = require ('node-accountkit');
 ```
 **Step 2** Configure account kit.
 ```javascript
 accountkit.set ("API_VERSION", "APP_ID", "ACCOUNT_KIT_APP_SECRET");
-// all info can be found on developer site at facebook 
-accountkit.requireAppSecret (true);// if you have enabled this option
+accountkit.requireAppSecret (true); // if you have enabled this option, default = true
 ```
-[https://developers.facebook.com/apps/](https://developers.facebook.com/apps/)
+More information [https://developers.facebook.com/apps/](https://developers.facebook.com/apps/)
 
 
 **Step 4** Use it.
-user data can be fetched inside the post request which client have submitted after account kit login.
-AngularJs helper library can be seen here [https://github.com/sgoyalnet/ng-account-kit](https://github.com/sgoyalnet/ng-account-kit)
 ```javascript
-//accountKitState and accountKitCode are the response thet we get from account kit login operation. look for sample app for more usage information.
-accountkit.getUserData (res, req.body.accountKitState, req.body.accountKitCode, function(resp) {
-  res.send (resp);
+//authorization_code are the authorizaition code that we get from account kit login operation. look for sample app for more usage information.
+accountkit.getAccountInfo (authorization_code, function(err, resp) {
     /**
     {
         "email": {
@@ -57,19 +40,23 @@ accountkit.getUserData (res, req.body.accountKitState, req.body.accountKitCode, 
     }
     */
 });
-// deleting user data
-//id is account-kit user id
-accountkit.deleteUser(req.query.id, function(resp){
-	res.send (resp);
+// Account Removal
+//accountId is accountkit user id
+accountkit.removeUser(accountId, function(err, resp){
+    /**
+    {
+        "success": true
+    }
+    */
 });
 ```
 
 ### Methods
 
-* accountkit.getUserData (response, accountKitState, accountKitCode, callback);
-* deleteUser (id, callback);
+* accountkit.getAccountInfo (authorization_code, callback);
+* removeUser (id, callback);
 
 ### Contribute
 
-Do you want to improve it? Sounds cool. Please drop me a line at <mail.goyalshubham@gmail.com>
+Feel free to fork it on Github :)
 
--- index.js
+++ index.js
@@ -4,92 +4,111 @@ var Querystring = require('querystring');
 var crypto = require('crypto');
 
 function AccountKit() {
-	var csrf_guid = Guid.raw();
-	var api_version = "";
-	var app_id = "";
-	var app_secret = '';
-	var require_app_secret = false;
-	var base_url = 'https://graph.accountkit.com/';
-	return {
-		set: function(v, id, secret) {
-			api_version = v;
-			app_id = id;
-			app_secret = secret;
-		},
-		requireAppSecret: function(_require_app_secret) {
-			require_app_secret = _require_app_secret;
-		},
-		getRandomState: function() {
-			return csrf_guid;
-		},
-		getApiVersion: function() {
-			return api_version;
-		},
-		getAppId: function() {
-			return app_id;
-		},
-		getAppSecret: function() {
-			return app_secret;
-		},
-		getEndpoint: function() {
-			return base_url + api_version + "/me";
-		},
-		getDelUrl: function(id) {
-			return base_url + api_version + '/' + id;
-		},
-		getTokenUrl: function() {
-			return base_url + api_version + "/access_token";
-		},
-		getUserData: function(response, accountKitState, accountKitCode, callback) {
-			var self = this;
-			// CSRF check
-			if (accountKitState === csrf_guid) {
-				var app_access_token = ['AA', app_id, app_secret].join('|');
+  var csrf_guid = Guid.raw();
+  var api_version = "v1.1";
+  var app_id = "";
+  var app_secret = "";
+  var require_app_secret = true;
+  var base_url = "https://graph.accountkit.com/";
+  return {
+    set: function(v, id, secret) {
+      api_version = v;
+      app_id = id;
+      app_secret = secret;
+    },
+    requireAppSecret: function(_require_app_secret) {
+      require_app_secret = _require_app_secret;
+    },
+    getRandomState: function() {
+      return csrf_guid;
+    },
+    getApiVersion: function() {
+      return api_version;
+    },
+    getAppId: function() {
+      return app_id;
+    },
+    getAppSecret: function() {
+      return app_secret;
+    },
+    getAppAccessToken: function() {
+      return ['AA', app_id, app_secret].join('|');
+    },
+    getEndpoint: function() {
+      return base_url + api_version + "/me";
+    },
+    getDelUrl: function(id) {
+      return base_url + api_version + '/' + id;
+    },
+    getTokenUrl: function() {
+      return base_url + api_version + "/access_token";
+    },
+    getAccountInfo: function(authorization_code, callback) {
+      var self = this;
 
-				var params = {
-					grant_type: 'authorization_code',
-					code: accountKitCode,
-					access_token: app_access_token
-				};
+      var params = {
+        grant_type: 'authorization_code',
+        code: authorization_code,
+        access_token: this.getAppSecret(),
+      };
 
-				// exchange tokens
-				var token_exchange_url = this.getTokenUrl() + '?' + Querystring.stringify(params);
-				//console.log (token_exchange_url);
-				Request.get({
-					url: token_exchange_url,
-					json: true
-				}, function(err, resp, respBody) {
-					if (err)
-						console.log(err);
-					// get account details at /me endpoint
-					var me_endpoint_url = self.getEndpoint() + '?access_token=' + respBody.access_token;
+      var token_exchange_url = this.getTokenUrl() + '?' + Querystring.stringify(params);
+      Request.get({
+        url: token_exchange_url,
+        json: true
+      }, function(error, resp, respBody) {
+        if (error) {
+          return callback(error);
+        } else if (respBody.error) {
+          return callback(respBody.error);
+        } else if (resp.statusCode !== 200) {
+          var errorMsg = "Invalid AccountKit Graph API status code (" + resp.statusCode + ")";
+          return callback(errorMsg);
+        }
 
-					if (require_app_secret)
-						me_endpoint_url += '&appsecret_proof=' + crypto.createHmac('sha256', app_secret).update(respBody.access_token).digest('hex');
-					console.log(me_endpoint_url);
-					Request.get({
-						url: me_endpoint_url,
-						json: true
-					}, function(err, resp, respBody) {
-						callback(respBody);
-					});
-				});
-			} else {
-				callback(undefined);
-			}
-		},
-		deleteUser: function(id, callback) {
-			var self = this;
-			var app_access_token = ['AA', app_id, app_secret].join('|');
-			var delUrl = this.getDelUrl(id) + "?" + "access_token=" + app_access_token;
-			Request.del({
-				url: delUrl,
-				json: true
-			}, function(err, resp, respBody) {
-				callback(respBody);
-			});
-		}
-	};
+        var me_endpoint_url = self.getEndpoint() + '?access_token=' + respBody.access_token;
+        if (require_app_secret) {
+          me_endpoint_url += '&appsecret_proof=' + crypto.createHmac('sha256', app_secret).update(respBody.access_token).digest('hex');
+        }
+
+        Request.get({
+          url: me_endpoint_url,
+          json: true
+        }, function(err, resp, respBody) {
+          if (error) {
+            return callback(error);
+          } else if (respBody.error) {
+            return callback(respBody.error);
+          } else if (resp.statusCode !== 200) {
+            var errorMsg = "Invalid AccountKit Graph API status code (" + resp.statusCode + ")";
+            return callback(errorMsg);
+          }
+
+          return callback(null, respBody);
+        });
+      });
+    },
+    removeUser: function(id, callback) {
+      var self = this;
+      var delUrl = this.getDelUrl(id) + "?" + "access_token=" + this.getAppSecret();
+
+      Request.del({
+        url: delUrl,
+        json: true
+      }, function(err, resp, respBody) {
+        if (error) {
+          return callback(error);
+        } else if (respBody.error) {
+          return callback(respBody.error);
+        } else if (resp.statusCode !== 200) {
+          var errorMsg = "Invalid AccountKit Graph API status code (" + resp.statusCode + ")";
+          return callback(errorMsg);
+        }
+
+        return callback(null, respBody);
+      });
+    }
+  };
 }
 
 module.exports = new AccountKit();
